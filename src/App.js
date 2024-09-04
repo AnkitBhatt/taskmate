9f3c5d5cbf6ba8d46f8a7bdc8754da62cf55d289
AnkitBhatt <ankitbhatt5597@gmail.com>
2024-09-04 16:02:15 +0530

    useState introduction and simple explanation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import { Header } from './components/Header';
-import  Footer  from './components/Footer';
+import { useState } from 'react';
 import './App.css';
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
+  function handleAdd(){
+    //state variable will stay constant in this unless you use function inside
+    setCount(count + 1)
+    setCount(count + 1)
+    //above line will increase only 1 in our counter, 
+    //from the next line if you use counter function than it will increase as many time you want
+    setCount(count => count + 1)
+  }
+
   return (
-    <>
-      <Header />
-      <div>
-        <h1>Ankit</h1>
+    <div className='App'>
+      <div className="box">
+        <p>{count}</p>
+        <button onClick={handleAdd} className='add'>ADD</button>
+        <button onClick={() => setCount(count - 1)} className="sub">SUB</button>
       </div>
-      <Footer />
-    </>
+    </div>
   )
 }
 
