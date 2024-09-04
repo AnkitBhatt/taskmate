bbbdaac448edc54c2d680eba2e12577f98fcd9e1
AnkitBhatt <ankitbhatt5597@gmail.com>
2024-09-04 17:02:55 +0530

    additional list added to showcase useState better

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
 
 export default function App() {
-  const [count, setCount] = useState(0);
+  const [tasks, setTasks] = useState([
+    {id:5271, name:"Start Learning React", completed:true},
+    {id:7825, name:"Practise side by side", completed:false},
+    {id:8391, name:"Comment after each video", completed:false}
+  ]);
 
-  function handleAdd(){
-    //state variable will stay constant in this unless you use function inside
-    setCount(count + 1)
-    setCount(count + 1)
-    //above line will increase only 1 in our counter, 
-    //from the next line if you use counter function than it will increase as many time you want
-    setCount(count => count + 1)
+  const [show, setShow] = useState(true);
+
+  function handleDelete(id){
+    setTasks(tasks.filter(task => task.id !== id))
   }
 
   return (
     <div className='App'>
-      <div className="box">
-        <p>{count}</p>
-        <button onClick={handleAdd} className='add'>ADD</button>
-        <button onClick={() => setCount(count - 1)} className="sub">SUB</button>
-      </div>
+      <h1>Task List</h1>
+      <ul>
+        <button className="trigger" onClick={() => setShow(!show)}>Toggle</button>
+        {show && tasks.map((task) => (
+          <li key={task.id} className={task.completed ? "completed" : "incomplete"}>
+            <span>{task.id} - {task.name}</span>
+            <button onClick={() => handleDelete(task.id)} className='delete'>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
-
