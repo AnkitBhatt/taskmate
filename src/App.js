910c9c4477d26e454c894f9bb206d832d17cd7be
AnkitBhatt <ankitbhatt5597@gmail.com>
2024-09-06 18:20:36 +0530

    TaskMate initial development is completed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-
+import { useState } from 'react';
 import { Header } from './components/Header';
 import { TaskList } from './components/TaskList';
 import { Footer } from './components/Footer';
+import { AddTask } from './components/AddTask';
 import './App.css'
 
 export default function App() {
 
   const info = "Info Prop Drilling"
+  const [tasks, setTasks] = useState([]);
 
   return (
     <div className='App'>
       <Header />
-      <TaskList info={info} title="Task List"/>
+      <main>
+        <AddTask tasks={tasks} setTasks={setTasks} />
+        <TaskList info={info} title="Task List" tasks={tasks} setTasks={setTasks} />
+      </main>
       <Footer />
     </div>
   )
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,23 +1,10 @@
 import { useState } from 'react'
 import { TaskCard } from './TaskCard';
-import { BoxCard } from './BoxCard';
 import "./TaskList.css"
 
-export const TaskList = ({title, info}) => {
-  const [tasks, setTasks] = useState([
-    {id:5271, name:"Start Learning React", completed:true},
-    {id:7825, name:"Practise side by side", completed:false},
-    {id:8391, name:"Comment after each video", completed:false}
-  ]);
+export const TaskList = ({title, info, tasks, setTasks}) => {
 
   const [show, setShow] = useState(true);
-  const styles = {
-    color: show ? "#3D8361" : "#be3434", 
-    border:"2px solid",
-    borderColor: show ? "#3D8361" : "#be3434", 
-    padding:"20px",
-    borderRadius: "5px"
-  }
 
   function handleDelete(id){
     setTasks(tasks.filter(task => task.id !== id))
@@ -25,25 +12,15 @@ export const TaskList = ({title, info}) => {
 
   return (
     <section className='tasklist'>
-      <h1 style={styles}>{title}</h1>
       <ul>
-        <button className="trigger" onClick={() => setShow(!show)}>{show ? "Hide" : "Show"}</button>
+        <div className='taskheader'>
+          <h1>{title}</h1>
+          <button className="trigger" onClick={() => setShow(!show)}>{show ? "Hide Tasks" : "Show Tasks"}</button>
+        </div>
         {show && tasks.map((task) => (
           <TaskCard info={info} key={task.id} task={task} handleDelete={handleDelete}/>
         ))}
       </ul>
-      <BoxCard result="success">
-        <p className="title">Lorem ipsum dolor sit amet.</p>
-        <p className="description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores et deleniti repellendus porro sint dolores, reiciendis id ab minima. Molestiae, eveniet ratione. Fugiat accusamus voluptatem beatae facere qui nulla libero.</p>
-      </BoxCard>
-      <BoxCard result="warning">
-        <p className="title">Lorem, ipsum.</p>
-        <p className="description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores et deleniti repellendus porro sint dolores, reiciendis id ab minima. Molestiae, eveniet ratione. Fugiat accusamus voluptatem beatae facere qui nulla libero.</p>
-      </BoxCard>
-      <BoxCard result="alert">
-        <p className="title">Lorem, ipsum dolor.</p>
-        <p className="description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores et deleniti repellendus porro sint dolores, reiciendis id ab minima. Molestiae, eveniet ratione. Fugiat accusamus voluptatem beatae facere qui nulla libero.</p>
-      </BoxCard>
     </section>
   )
 }
